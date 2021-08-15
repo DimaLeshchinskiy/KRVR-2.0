cf1d1ceaa08c9a1d9ec2794fddb40c43d935f678
Dima <dmitryl0626@gmail.com>
2021-08-15 13:38:57 +0200

    update .bat sripts for merge JS to Python project; remove Java merge .bat file

--- buildJS.bat
+++ buildJS.bat
@@ -1,3 +1,6 @@
 cd ./KRVRJS
 start /separate /wait cmd /c npm run build
 cd ..
+
+echo y | rmdir .\\KRVRSW3\\public\\ /s
+echo y | Xcopy .\\KRVRJS\\build .\\KRVRSW3\\public\\ /E /H /C /I
--- buildJava.bat
+++
@@ -1,4 +0,0 @@
-echo y | rmdir .\\KRVRSW\\src\\main\\resources\\static\\ /s
-echo y | Xcopy .\\KRVRJS\\build .\\KRVRSW\\src\\main\\resources\\static\\ /E /H /C /I
-cd ./KRVRSW
-mvn clean install
