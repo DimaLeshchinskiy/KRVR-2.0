7058ebc3625461492f6613509da72db2716347a1
Dima <dmitryl0626@gmail.com>
2021-08-23 17:40:42 +0200

    export Ext and Int perimeter action process

--- KRVRSW/core/__init__.py
+++ KRVRSW/core/__init__.py
@@ -4,3 +4,5 @@ from .communication.SerialCommunication import SerialCommunication
 
 from .actionProcess.FaceActionProcess import FaceActionProcess
 from .actionProcess.PocketActionProcess import PocketActionProcess
+from .actionProcess.ExtPerimeterActionProcess import ExtPerimeterActionProcess
+from .actionProcess.IntPerimeterActionProcess import IntPerimeterActionProcess
