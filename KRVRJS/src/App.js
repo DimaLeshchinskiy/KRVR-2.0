5dcd0a34482b1763c72de8339851c782f4283296
Dima <dmitryl0626@gmail.com>
2021-09-01 22:33:27 +0200

    use Socket, Port contexts in App.js

--- KRVRJS/src/App.js
+++ KRVRJS/src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
+import "./App.css";
 
-import LeftBar from "./components/LeftBar/LeftBar.js";
-import RightBar from "./components/RightBar/RightBar.js";
-import MainSpace from "./components/MainSpace/MainSpace.js";
-import FileDrop from "./components/FileDrop/FileDrop.js";
+import LeftBar from "@components/LeftBar/LeftBar.js";
+import RightBar from "@components/RightBar/RightBar.js";
+import MainSpace from "@components/MainSpace/MainSpace.js";
+import FileDrop from "@components/FileDrop/FileDrop.js";
 
-import React from 'react';
+import React from "react";
 
-import {FileContextProvider} from "./context/file";
+import { FileContextProvider } from "@context/file";
+import { PortContextProvider } from "@context/port";
+import { SocketContextProvider } from "@context/socket";
 
 import useDebounce from "./hooks/useDebounce.js";
 
 function App() {
-
-/*
+  /*
   const callAPI = async (value) => {
     console.log("HARD API");
   };
@@ -25,13 +26,17 @@ function App() {
   };
 */
   return (
-    <FileContextProvider>
-      <FileDrop>
-          <LeftBar / >
-          <MainSpace / >
-          <RightBar / >
-      </FileDrop>
-    </FileContextProvider>
+    <SocketContextProvider>
+      <FileContextProvider>
+        <PortContextProvider>
+          <FileDrop>
+            <LeftBar />
+            <MainSpace />
+            <RightBar />
+          </FileDrop>
+        </PortContextProvider>
+      </FileContextProvider>
+    </SocketContextProvider>
   );
 }
 
