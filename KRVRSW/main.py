caf657684da42e6e0661c9182b4e2f5cec3695ac
Dima <dmitryl0626@gmail.com>
2021-09-01 22:25:26 +0200

    refactor startup file main.py

--- KRVRSW/main.py
+++ KRVRSW/main.py
@@ -1,15 +1,5 @@
 import webview
 from app import app
 
-
-def after_start(window):
-    # window.toggle_fullscreen()
-    pass
-
-
-if __name__ == '__main__':
-    window = webview.create_window('KRVR',
-                                   app,
-                                   min_size=(1280, 720),
-                                   fullscreen=False)
-    webview.start(after_start, window, debug=True)
+webview.create_window('KRVR', app)
+webview.start(debug=True)
