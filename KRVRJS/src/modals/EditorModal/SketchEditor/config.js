bfddbdcd22702a54d62041e2eea94576e596a3d0
Dima <dmitryl0626@gmail.com>
2021-10-23 09:21:53 +0200

    edit sketchEditor config

--- KRVRJS/src/modals/EditorModal/SketchEditor/config.js
+++ KRVRJS/src/modals/EditorModal/SketchEditor/config.js
@@ -80,35 +80,55 @@ export default {
         </svg>
       ),
     },
+    // {
+    //   id: 2,
+    //   title: "circle",
+    //   svg: (
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       width="16"
+    //       height="16"
+    //       fill="currentColor"
+    //       class="bi bi-circle"
+    //       viewBox="0 0 16 16"
+    //     >
+    //       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+    //     </svg>
+    //   ),
+    // },
+    // {
+    //   id: 3,
+    //   title: "rectangle",
+    //   svg: (
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       width="16"
+    //       height="16"
+    //       fill="currentColor"
+    //       class="bi bi-square"
+    //       viewBox="0 0 16 16"
+    //     >
+    //       <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+    //     </svg>
+    //   ),
+    // },
     {
-      id: 2,
-      title: "circle",
+      id: 4,
+      title: "delete",
       svg: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-circle"
+          class="bi bi-trash"
           viewBox="0 0 16 16"
         >
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-        </svg>
-      ),
-    },
-    {
-      id: 3,
-      title: "rectangle",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-square"
-          viewBox="0 0 16 16"
-        >
-          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+          <path
+            fill-rule="evenodd"
+            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+          />
         </svg>
       ),
     },
